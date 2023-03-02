3507e51d3b24b61203cb689af590b11554a878bb
gosekose <ksyn1611@naver.com>
2023-03-02 22:33:07 +0900

    feat: @ConfigurationProperties 적용

--- external-read/src/main/java/hello/ExternalReadApplication.java
+++ external-read/src/main/java/hello/ExternalReadApplication.java
@@ -1,9 +1,20 @@
 package hello;
 
+import hello.config.MyDataSourcePropertiesConfigV1;
+import hello.config.MyDataSourcePropertiesConfigV2;
+import hello.config.MyDataSourcePropertiesConfigV3;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+//@Import(MyDataSourceEnvConfig.class)
+//@Import(MyDataSourceValueConfig.class)
+//@Import(MyDataSourcePropertiesConfigV1.class)
+//@Import(MyDataSourcePropertiesConfigV2.class)
+@Import(MyDataSourcePropertiesConfigV3.class)
+@SpringBootApplication(scanBasePackages = "hello.datasource")
+//@ConfigurationPropertiesScan({"hello"})
 public class ExternalReadApplication {
 
     public static void main(String[] args) {
---
+++ external-read/src/main/java/hello/datasource/MyDataSourcePropertiesV3.java
@@ -0,0 +1,45 @@
+package hello.datasource;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.hibernate.validator.constraints.time.DurationMax;
+import org.hibernate.validator.constraints.time.DurationMin;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+import java.lang.annotation.Native;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+@ConfigurationProperties("my.datasource")
+@Validated
+public class MyDataSourcePropertiesV3 {
+
+    @NotNull
+    private String url;
+    @NotNull
+    private String username;
+    @NotNull
+    private String password;
+    @NotNull
+    private Etc etc;
+
+    @Getter
+    @AllArgsConstructor
+    public static class Etc {
+        @Min(1)
+        @Max(999)
+        private int maxConnection;
+
+        @DurationMin(seconds = 1)
+        @DurationMax(seconds = 60)
+        private Duration timeout;
+        private List<String> options = new ArrayList<>();
+    }
+}
